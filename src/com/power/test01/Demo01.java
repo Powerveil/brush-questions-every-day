3faec91e8582c21003b6755cafbaba8cfb46db3c
Powercs12 <1968103220@qq.com>
2022-10-20 22:43:09 +0800

    学习

---
+++ src/com/power/test01/Demo01.java
@@ -0,0 +1,33 @@
+package com.power.test01;
+
+import java.util.*;
+
+/**
+ * @author 崔帅
+ * @version 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        Random random = new Random(0);
+        System.out.println("nextInt():" + random.nextInt());
+        System.out.println("nextInt():" + random.nextInt());
+        System.out.println("nextInt(int bound):" + random.nextInt(5));
+        System.out.println("nextBoolean():" + random.nextBoolean());
+        System.out.println("nextDouble():" + random.nextDouble());
+        System.out.println("nextFloat():" + random.nextFloat());
+
+        byte[] bytes = new byte[4096];
+        random.nextBytes(bytes);
+        System.out.println("nextBytes后byte" + Arrays.toString(bytes));
+        System.out.println("bytes[4095]:" + bytes[4095]);
+        System.out.println("nextLong():" + random.nextLong());
+        System.out.println("nextGaussian():" + random.nextGaussian());
+        Collection<String> list = new ArrayList<>();
+        list.add("nihao");
+        list.add("nihao");
+        list.add("nihao");
+        System.out.println(list);
+//        LinkedList
+
+    }
+}
