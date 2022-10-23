b578af3d07ca9f96b2283a1a1d002b39fb9914ae
Powercs12 <1968103220@qq.com>
2022-10-23 23:57:58 +0800

    学习

--- src/com/power/test01/Demo02.java
+++ src/com/power/test01/Demo02.java
@@ -9,17 +9,34 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Demo02 {
     public static void main(String[] args) {
 //        ConcurrentHashMap
+        System.out.println(findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
     }
 
-    public int findRepeatNumber(int[] nums) {
+//    public int findRepeatNumber(int[] nums) {
+//        int[] arr = new int[nums.length];
+//        for (int num : nums) {
+//            arr[num]++;
+//        }
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 1) return i;
+//        }
+//        return -1;
+//    }
+
+    public static int findRepeatNumber(int[] nums) {
         int[] arr = new int[nums.length];
         for (int num : nums) {
+            if (arr[num] > 0) return num;
             arr[num]++;
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 1) return i;
         }
         return -1;
     }
+
+
+//    public int findRepeatNumber(int[] nums) {
+//        int[] arr = new int[nums.length];
+//        for (int num : nums) if (arr[num]++ > 0) return num - 1;
+//        return -1;
+//    }
 }
