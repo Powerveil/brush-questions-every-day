80db2188f142945107d85f78d2881676a12548d7
Powercs12 <1968103220@qq.com>
2022-10-28 22:22:18 +0800

    学习

---
+++ src/com/power/test01/PerfectNum.java
@@ -0,0 +1,29 @@
+package com.power.test01;
+
+import java.util.Scanner;
+
+public class PerfectNum {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int a = 0;
+//        for (int i = 1; i <= n; i++) {
+//            int j = 1;
+//            int sum = 0;
+//            while (j < i) {
+//                if (i % j == 0) {
+//                    sum += j;
+//                }
+//                j++;
+//            }
+//            if (i == sum) {
+//                a += 1;
+//            }
+//        }
+//        System.out.println(a);
+
+
+
+        System.out.println("张三".length());
+    }
+}
