1f69fd7a56f51759e9b61a698d397fb33ab18fa7
Powercs12 <1968103220@qq.com>
2022-11-02 11:12:56 +0800

    异常练习

--- src/com/power/test01/Demo5.java
+++ src/com/power/test01/Demo5.java
@@ -5,5 +5,26 @@ package com.power.test01;
  * @version 1.0
  */
 public class Demo5 {
+    public static void main(String[] args) {
 
+        try {
+            int a = 5 / 0;
+        } catch (ArithmeticException e) {
+//            e.printStackTrace();
+            System.out.println("除数不能为0");
+        }
+
+        int[] arr = new int[3];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        try {
+            System.out.println(arr[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+//            e.printStackTrace();
+            System.out.println("数组访问越界");
+        }
+
+    }
 }
