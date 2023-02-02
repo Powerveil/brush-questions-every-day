91122aa75d8323923c850dc5bbf53b745041d2ca
Powercs12 <1968103220@qq.com>
2023-02-02 20:50:09 +0800

    刷题

--- src/com/power/study_2023_2/Demo01.java
+++ src/com/power/study_2023_2/Demo01.java
@@ -1,5 +1,7 @@
 package com.power.study_2023_2;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -144,24 +146,122 @@ public class Demo01 {
         return mergeList(lists, 0, lists.size() - 1);
     }
 
-    public ListNode mergeList(List<ListNode> lists, int left, int right) {
+    private ListNode mergeList(List<ListNode> lists, int left, int right) {
         if (left == right) return lists.get(left);
         if (left > right) return null;
         int mid = left + (right - left) / 2;
         return merge(mergeList(lists, left, mid), mergeList(lists, mid + 1, right));
     }
 
-    //合并两个有序链表（和力扣的21题一样）
-    public ListNode merge(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        if (l1.val < l2.val) {
-            l1.next = merge(l1.next, l2);
-            return l1;
+    public ListNode merge(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val < list2.val) {
+            list1.next = merge(list1.next, list2);
+            return list1;
         } else {
-            l2.next = merge(l1, l2.next);
-            return l2;
+            list2.next = merge(list1, list2.next);
+            return list2;
         }
+    }
+
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null || pHead.next == null) return null;
+        ListNode slow = pHead;
+        ListNode fast = pHead;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null) return null;
+        ListNode start  = pHead;
+        while (true) {
+            if (start == slow) return start;
+            start = start.next;
+            slow = slow.next;
+        }
+    }
+
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param pHead ListNode类
+     * @param k int整型
+     * @return ListNode类
+     */
+    public ListNode FindKthToTail (ListNode pHead, int k) {
+        // write code here
+        if (k <= 0) return null;
+        ListNode cur = pHead;
+        int count = 0;
+        while (cur != null) {
+            cur = cur.next;
+            count++;
+        }
+        if (k > count) return null;
+        count -= k;
+        cur = pHead;
+        while (count > 0) {
+            cur = cur.next;
+            count--;
+        }
+        return cur;
+    }
+
+
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        if (pHead1 == pHead2) return pHead1;
+        if (pHead1 == null || pHead2 == null) return null;
+        int count1 = 0;
+        int count2 = 0;
+        ListNode cur1 = pHead1;
+        ListNode cur2 = pHead2;
+        while (cur1 != null) {
+            cur1 = cur1.next;
+            count1++;
+        }
+        while (cur2 != null) {
+            cur2 = cur2.next;
+            count2++;
+        }
+        cur1 = pHead1;
+        cur2 = pHead2;
+        int count = 0;
+        if (count1 > count2) {
+            count = count1 - count2;
+            while (count > 0) {
+                cur1 = cur1.next;
+                count--;
+            }
+        } else {
+            count = count2 - count1;
+            while (count > 0) {
+                cur2 = cur2.next;
+                count--;
+            }
+        }
+        while (cur1 != null && cur2 != null) {
+            if (cur1 == cur2) return cur1;
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return null;
     }
 
 }
